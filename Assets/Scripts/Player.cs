7572bb7428fb41ab55ddf90122ea6c107be8f55f
Haim Schnitzer <ziknapo@gmail.com>
2024-01-26 02:45:24 +0200

    Improve player control

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public GameObject aimArrow;
+    public GameObject yarnPiece;
 private CameraController cameraController;
 private Rigidbody rigidBody;
 
@@ -30,13 +31,39 @@ public void RotateAimArrow(float xAxis, float yAxis)
 public void ReleaseYarn()
 {
     int layerMask = ~gameObject.layer;
+     float depth = 0.2f;
     RaycastHit hit;
      if (Physics.Raycast(transform.position, aimArrow.transform.forward, out hit, Mathf.Infinity, layerMask))
-        {
-        float power= 500f*Mathf.Sqrt(hit.distance);
-         rigidBody.AddForce(aimArrow.transform.forward*power);
-        }
+    {
+           float power= 500f*Mathf.Sqrt(hit.distance);
+            StartCoroutine(ShootYarn((int)Mathf.Floor(hit.distance/depth),power));
+    }
 
+}
+
+private IEnumerator ShootYarn(int pieceCount,float power)
+{
+   float depth = 0.2f;
+   Vector3 dir=aimArrow.transform.forward;
+   Quaternion aimArrowrRotation = aimArrow.transform.rotation;
+   Vector3 startPos = transform.position;
+   for(int i=0; i<pieceCount;i++)
+   {
+   Instantiate(yarnPiece,startPos+dir*depth*i,aimArrowrRotation);
+   if(pieceCount<50)
+   {
+    if(i%2==0)
+    {
+       yield return null;
+    }
+   }
+   else if(i%7==0)   
+   {
+       yield return null;
+   }
+   }
+       yield return null;
+   rigidBody.AddForce(dir*power);
 }
 
 
