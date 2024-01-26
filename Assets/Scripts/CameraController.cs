3870b9b6b38dc0a834eed6e138f32c9abe7c9f82
Haim Schnitzer <ziknapo@gmail.com>
2024-01-26 16:15:07 +0200

    Fix x axis cam rotation

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -68,8 +68,9 @@ public class CameraController : MonoBehaviour
         {
         SlideToRotation(newAngleX-Input.GetAxis("Mouse Y") * 5f,newAngleY+Input.GetAxis("Mouse X") * 5f);
         }
-        Vector3 rotatedOffset = Quaternion.AngleAxis(-currentAngleX, Vector3.left) * new Vector3(1,1,offset.z);
+        Vector3 rotatedOffset = Quaternion.AngleAxis(-currentAngleX, Vector3.left) * new Vector3(1,offset.y,offset.z);
         rotatedOffset+= Quaternion.AngleAxis(currentAngleY, Vector3.up) * new Vector3(1,offset.y,offset.z);
+        rotatedOffset-= offset;
         transform.position = Vector3.Lerp(transform.position, newTarget + rotatedOffset, moveSpeed * Time.deltaTime);
         currentAngleY = Mathf.Lerp(currentAngleY, newAngleY, rotateSpeed * Time.deltaTime);
         currentAngleX = Mathf.Lerp(currentAngleX, newAngleX, rotateSpeed * Time.deltaTime);
